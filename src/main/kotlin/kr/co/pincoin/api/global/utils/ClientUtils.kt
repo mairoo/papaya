d3430042bba3cf634ed2dbd1c91a6041b709ca9e
mairoo <jonghwa@pincoin.co.kr>
2025-01-06 23:48:28 +0900

    ip utils

--- src/main/kotlin/kr/co/pincoin/api/global/utils/ClientUtils.kt
+++ src/main/kotlin/kr/co/pincoin/api/global/utils/ClientUtils.kt
@@ -2,6 +2,8 @@ package kr.co.pincoin.api.global.utils
 
 import jakarta.servlet.http.HttpServletRequest
 
+// 싱글톤 구현으로 단 하나의 인스턴스만 존재
+// 자바에서 모든 메소드가 public static 메소드로 모은 유틸리티 함수, 상수애 대응
 object ClientUtils {
     private const val USER_AGENT_HEADER = "User-Agent"
     private const val ACCEPT_LANGUAGE_HEADER = "Accept-Language"
