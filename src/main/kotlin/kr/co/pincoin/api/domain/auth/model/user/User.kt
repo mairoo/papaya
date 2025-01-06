2c6a90c735d0df29f088fc90d931008361038ba0
mairoo <jonghwa@pincoin.co.kr>
2025-01-06 21:47:13 +0900

    factory method

--- src/main/kotlin/kr/co/pincoin/api/domain/auth/model/user/User.kt
+++ src/main/kotlin/kr/co/pincoin/api/domain/auth/model/user/User.kt
@@ -4,21 +4,23 @@ import kr.co.pincoin.api.app.member.user.request.UserCreateRequest
 import java.time.LocalDateTime
 
 class User private constructor(
-    // 식별자 및 생성일 (불변)
+    // 불변
+    // 식별자 및 가입일시
     val id: Int?,
     val dateJoined: LocalDateTime,
 
-    // 인증 관련 (가변)
+    // 가변
+    // 인증정보
     var password: String,
     val email: String,
     var lastLogin: LocalDateTime?,
 
-    // 개인정보 (가변)
+    // 개인정보
     val username: String,
     var firstName: String,
     var lastName: String,
 
-    // 권한 관련 (가변)
+    // 권한 관련
     var isSuperuser: Boolean,
     var isStaff: Boolean,
     var isActive: Boolean,
@@ -50,44 +52,14 @@ class User private constructor(
             isActive = isActive
         )
 
-        fun createUser(
-            email: String,
-            password: String,
-            username: String,
-            firstName: String = "",
-            lastName: String = "",
-        ): User = of(
-            email = email,
-            password = password,
-            username = username,
-            firstName = firstName,
-            lastName = lastName
+        fun from(request: UserCreateRequest, isAdmin: Boolean = false): User = of(
+            email = request.email,
+            password = request.password,
+            username = request.username,
+            firstName = request.firstName,
+            lastName = request.lastName,
+            isSuperuser = isAdmin,
+            isStaff = isAdmin
         )
-
-        fun createAdmin(
-            email: String,
-            password: String,
-            username: String,
-            firstName: String = "",
-            lastName: String = "",
-        ): User = of(
-            email = email,
-            password = password,
-            username = username,
-            firstName = firstName,
-            lastName = lastName,
-            isSuperuser = true,
-            isStaff = true
-        )
-
-        fun from(request: UserCreateRequest): User {
-            return of(
-                email = request.email,
-                password = request.password,
-                username = request.username,
-                firstName = request.firstName,
-                lastName = request.lastName
-            )
-        }
     }
 }
